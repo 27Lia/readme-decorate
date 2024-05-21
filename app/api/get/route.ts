b1d6251b0a7ee4654f80b714e49a4b9cc9e45881
27Lia <117743861+27Lia@users.noreply.github.com>
2024-05-21 22:58:35 +0900

    get api 수정

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -1,30 +1,35 @@
 // app/api/get/route.ts
 
-
+import GradientSvg from '@/app/components/GradientSvg';
 import StrokeSvg from '@/app/components/StrokeSvg';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-    const { searchParams } = req.nextUrl;
-    const height = searchParams.get('height');
-    const text = searchParams.get('text');
-    const color = searchParams.get('color');
-    const type = searchParams.get('type');
-  
-   let svg;
-    if (type === "stroke") {
-      svg = StrokeSvg(color || "", height || "", text || "");
-    } 
+  const { searchParams } = req.nextUrl;
+  const height = searchParams.get('height') || "";
+  const text = searchParams.get('text') || "";
+  const fontColor = searchParams.get('fontColor') || "";
+  const fontSize = searchParams.get('fontSize') || "";
+  const backgroundColor = searchParams.get('backgroundColor') || "#FFFFFF";
+  const type = searchParams.get('type') || "rectangle";
+  const gradientColors = [searchParams.get('gradientColor1') || "", searchParams.get('gradientColor2') || ""];
 
-    else {
-      svg = `
-        <svg width="800" height="${height}" xmlns="http://www.w3.org/2000/svg">
-          <rect width="100%" height="100%" fill="${color}" />
-          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="white">${text}</text>
-        </svg>
-      `;
-    }
-  
+  let svg;
+
+  if (type === "stroke") {
+    svg = StrokeSvg(fontColor, backgroundColor, height, text, fontSize);
+  } else if (type === "gradient") {
+    svg = GradientSvg(fontColor, gradientColors, height, text, fontSize);
+  } else {
+    svg = `
+      <svg width="800" height="${height}" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="${backgroundColor}" />
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${fontSize}" fill="${fontColor}">${text}</text>
+      </svg>
+    `;
+  }
+
+
     return new NextResponse(svg, {
       headers: {
         'Content-Type': 'image/svg+xml',
