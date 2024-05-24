94debbe2f29db2e2894383ecb53eb1dc54fde0f5
27Lia <117743861+27Lia@users.noreply.github.com>
2024-05-24 09:57:07 +0900

    -

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ const Home: React.FC = () => {
     <div>
       {isMobile && showWarning && (
         <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-xs mt-5 p-3 bg-red-100 text-red-700 border border-red-400 rounded text-center z-50 transition-opacity duration-500">
-          이 콘텐츠는 모바일 기기에서 제대로 작동하지 않을 수 있습니다. 최상의
-          경험을 위해 데스크톱 웹 브라우저를 사용해 주세요.
+          모바일 기기에서 제대로 작동하지 않을 수 있습니다 웹 브라우저를 사용해
+          주세요.
         </div>
       )}
       <SvgRequest />
